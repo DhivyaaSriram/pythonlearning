91e7d0c931cdb858921eb00e7dc9f8e984aff30d
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-06-05 12:10:01 +0530

    user input based prolems

---
+++ dhivyaa.py
@@ -0,0 +1,33 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================ RESTART: D:/dhivyaa.py ============================
+enter the name S. Dhivyaa
+enter the address1 23, Mariamman Koil Street, Lawspet, 
+enter the address2 Puducherry-605008.
+enter the name The Class Incharge, 
+enter the address1 KMC,
+enter the address2 Puducherry - 605008.
+Sir/Madam Sir/Madam,
+no of days 3
+enter the place Puducherry.
+enter the date 05/06/2023.
+enter the name S. Dhivyaa 
+From
+
+ S. Dhivyaa
+ 23, Mariamman Koil Street, Lawspet, 
+ Puducherry-605008.
+
+ The Class Incharge, 
+ KMC,
+ Puducherry - 605008.
+
+Respected  Sir/Madam,
+
+	As I am suffering from fever, I am unable to attend the class. So, kindly grant me leave for  3 days.
+
+	 Thanking you
+
+Place:  Puducherry. 	Yours faithfully,
+Date:  05/06/2023. 	  S. Dhivyaa 
