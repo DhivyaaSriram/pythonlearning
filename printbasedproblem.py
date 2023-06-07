7332dc1d107ce12834693f6fe6e7cbf8b15a0448
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-06-07 09:48:45 +0530

    Add files via upload

---
+++ printbasedproblem.py
@@ -0,0 +1,12 @@
+print("---------------------------------------------------------")
+print("\t\t STUDENT INFORMATION")
+print("---------------------------------------------------------")
+print("\t NAME       : S.DHIVYAA")
+print("\t AGE        : 22")
+print("\t ADDRESS    : 23, Mariamman koil Street,Lawspet,SPuducherry - 605008")
+print("\t EMAIL      : dhivyaa2909@gmail.com")
+print("\t MOBILE     : ABCD")
+print("---------------------------------------------------------")
+
+
+
