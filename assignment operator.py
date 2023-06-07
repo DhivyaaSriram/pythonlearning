03e0820a722f4bfe02c302ee151b4d058c14586a
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-06-07 11:26:23 +0530

    Add files via upload

---
+++ assignment operator.py
@@ -0,0 +1,24 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: F:/Students☻/Dhivyaa/assignment operator.py =============
+-78
+-5.5
+1
+>>> 
+============= RESTART: F:/Students☻/Dhivyaa/assignment operator.py =============
+-78
+-5.5
+1
+(-7+5.6457841394062536e-11j)
+3.5
+(8-5.6457841394062536e-11j)
+>>> 
+============= RESTART: F:/Students☻/Dhivyaa/assignment operator.py =============
+-78
+-5.5
+1
+
+34289.447249000004
+10
+-34293.447249000004
