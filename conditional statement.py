e99d7a06c8b11039f0e191af320d62365f3d3797
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-06-12 11:17:27 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,25 @@
+##eligible to vote
+a=int(input("enter your age"))
+if(a>=18):
+    print("you are eligible to vote")
+else:
+   print("you are not eligible to vote")
+##Senior citizen
+b=int(input("enter the age"))
+if(b>=60):
+    print("The person is a senior citizen")
+else:
+    print("The person is not a senior citizen")
+##positive or negative
+c=int(input("enter the vale of c"))
+if(c>0):
+    print("the number is positive")
+else:
+    print("the number is negative")
+##odd or even
+d=int(input("enter the number"))
+if(d%2==0):
+    print("The given value is a even number")
+else:
+    print("the given value is a odd number")
+
