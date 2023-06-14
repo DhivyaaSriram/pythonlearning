c5e637d130b0eb5f2da1918d010411a6f85961d0
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-06-14 12:07:33 +0530

    Add files via upload

---
+++ CONDITIONAL STATEMENT TASK.py
@@ -0,0 +1,57 @@
+##2.Grade criteria
+a=int(input("enter your marks"))
+if a>90:
+    print("you are passed in A grade")
+elif a>80 and a<=90:
+    print("you are passed in a B grade")
+elif a>=60 and a<=80:
+    print("you are passed in a C grade")
+else:
+    print("You are passed in D grade")
+##4.Program to accept a number to display a day
+day=int(input("Enter the number from 1 to 7:"))
+if day==1:
+    print("the day is sunday")
+elif day==2:
+    print("The day is monday")
+elif day==3:
+    print("the day is tuesday")
+elif day==4:
+    print("the day is wednesday")
+elif day==5:
+    print("the day is thursday")
+elif day==6:
+    print("the day is friday")
+elif day==7:
+    print("the day is saturday")
+else:
+    print("it is invalid")
+    
+##5.Program to accept a number to display a month and no. of days
+month=int(input("enter the number from 1 to 12:"))
+if month==1:
+    print("The month is January and number of days is 31")
+elif month==2:
+    print("The month is February and number of days is 28")
+elif month==3:
+    print("The month is March and number of days is 31")
+elif month==4:
+    print("The month is April and number of days is 30")
+elif month==5:
+    print("The month is May and number of days is 31")
+elif month==6:
+    print("The month is June and number of days is 30")
+elif month==7:
+    print("The month is July and number of days is 31")
+elif month==8:
+    print("The month is August and number of days is 31")
+elif month==9:
+    print("The month is September and number of days is 30")
+elif month==10:
+    print("The month is October and number of days is 31")
+elif month==11:
+    print("The month is November and number of days is 30")
+elif month==12:
+    print("The month is December and number of days is 31")
+
+
