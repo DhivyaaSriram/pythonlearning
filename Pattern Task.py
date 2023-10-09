df6bb00cf053ee2b57cb2393e71fbfc7f75fca08
DhivyaaSriram <135583873+DhivyaaSriram@users.noreply.github.com>
2023-10-09 08:19:23 +0530

    Add files via upload

---
+++ Pattern Task.py
@@ -0,0 +1,99 @@
+##1)Star Pattern:
+##for i in range(1,6):
+##    for j in range(i):
+##        print("*",end=" ")
+##    print()
+##
+##2)Star Pattern:
+##n=int(input("Enter the number"))
+##for i in range(n):
+##    for j in range(n-i-1):
+##        print(" ", end="")
+##    for j in range(i+1):
+##        print("*",end=" ")
+##    print()
+##
+##3)
+##for i in range(1,6):
+##    print("A B C D E")
+##print()
+##
+##4)
+##letters = ['A', 'B', 'C', 'D', 'E']
+##for letter in letters:
+##    row = letter * 5
+##    print(row)
+##
+##5)
+##pattern = ["A B C D E", "F G H I J", "K L M N O", "P Q R S T", "U V W X Y"]
+##for row in pattern:
+##    print(row)
+##
+##6)
+##num=("0 1 0 1 0", "0 0 0 0 0","0 1 0 1 0","0 0 0 0 0", "0 1 0 1 0")
+##for row in num:
+##    print(row)
+##    
+##7)
+##num=("0 1 0 1 0", "1 0 1 0 1","0 1 0 1 0","1 0 1 0 1", "0 1 0 1 0")
+##for row in num:
+##    print(row)
+##
+##8)
+##num=("1 3 5 7 9", "3 5 7 9 11", "5 7 9 11 13", "7 9 11 13 15", "9 11 13 15 17")
+##for row in num:
+##    print(row)
+####
+
+##9)
+##for i in range(1,6):
+##    print("5 4 3 2 1")
+##print()
+    
+##10)
+##num_rows = 5
+##num_cols = 5
+##for i in range(1, num_rows * num_cols + 1):
+##    print(i, end=' ')
+##    if i % num_cols == 0:
+##        print()
+
+##11)
+##for row in range(2, 51, 10):
+##    for col in range(row, row + 10, 2):
+##        print(col, end=" ")
+##    print()
+
+
+##12)
+##for row in range(1,50,10):
+##    for col in range(row,row+10,2):
+##        print(col,end=" ")
+##    print()
+
+##13)
+##for i in range(1,6):
+##    for j in range(1,4):
+##        print(f"{i} {j}",end= " ")
+##    print()
+    
+##14)
+##for i in range(1,6):
+##    for j in range(1,4):
+##        print(f"{j} {i}",end= " ")
+##    print()
+
+##15)
+##for i in range(1,6):
+##    for j in range(i,i+21,5):
+##        print(j, end=" ")
+##    print()
+ 
+##16)
+##n=5
+##for i in range(n):
+##    for j in range(n):
+##        print(i+j+1, end=" ")
+##    print()
+
+ 
